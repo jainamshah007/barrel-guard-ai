d60102ca8ae6451604cef875bf34d50ab9d7aa6f
jainamshah007 <jainam_shah007@yahoo.com>
2026-05-27 17:41:37 +0530

    Fix Dockerfile COPY paths and requirements.txt filename case

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,9 @@
 // Copyright (c) 2024 Jainam K Shah. All Rights Reserved.
 // =============================================================
 
-// @ts-ignore
 import React from 'react'
-// @ts-ignore
 import ReactDOM from 'react-dom/client'
-// @ts-ignore
 import App from './App'
-// @ts-ignore
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
